08e8c0f555c2b9d0a73fabce74bc2fa7ee33e9da
Natan <natanrodrigo4@gmail.com>
2023-11-28 07:48:07 -0300

    Viloes para jogo rpg

---
+++ src/Monstro.java
@@ -0,0 +1,33 @@
+public class Monstro {
+    public  Monstro(String nome, int pontoDeVida , int ataqueFraco 100, int ataqueForte,int especial, int velocidade){
+        Monstro aberracaoSombria = new Monstro();
+        Monstro anciaoDosMares = new Monstro();
+        Monstro banshee = new Monstro();
+        Monstro cavaleiroDaMontanha = new Monstro();
+        Monstro crawler = new Monstro();
+        Monstro devoradorDeAlma = new Monstro();
+        Monstro devoradorDeMentes = new Monstro();
+        Monstro dracoDoAbismo = new Monstro();
+        Monstro duergar = new Monstro();
+        Monstro espectro = new Monstro();
+        Monstro espiritoArtomentado = new Monstro();
+        Monstro fadaNegra = new Monstro();
+        Monstro giganteDaMontanha = new Monstro();
+        Monstro golemDePedra = new Monstro();
+        Monstro gorgon = new Monstro();
+        Monstro guardiaoAquatico = new Monstro();
+        Monstro guardiaoDasProfundezas = new Monstro();
+        Monstro hidra = new Monstro();
+        Monstro leviata = new Monstro();
+        Monstro lich = new Monstro();
+        Monstro lordeDasSombra = new Monstro();
+        Monstro metamorfoDaCaverna = new Monstro();
+        Monstro murloc = new Monstro();
+        Monstro nagaDasTrevas = new Monstro();
+        Monstro necromante = new Monstro();
+        Monstro sirena = new Monstro();
+        Monstro tritao = new Monstro();
+        Monstro troglodita = new Monstro();
+        Monstro trollDasCavernas = new Monstro();
+    }
+}
